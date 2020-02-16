683aa133580f062e4dd0f3dd4154394ec8c7a374
M. Ayasy Al Muhandis <ayasyseijuro@gmail.com>
2020-02-16 13:00:38 +0700

    make it better

--- app/examples/insert.table.js
+++ app/examples/insert.table.js
@@ -1,6 +1,6 @@
 //app/examples/insert.data.js
 const sql = require("../models/db");
-sql.query("INSERT INTO books VALUES (NULL, 'title', 'desc', 'perpustakaan.png', 'current_timestamp'());", (err,res) => {
+sql.query("INSERT INTO books VALUES (NULL, 'title', 'desc', 'perpustakaan.png', current_timestamp());", (err,res) => {
     if(err){
         console.log(err);
     } else {
